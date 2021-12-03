456962422991c184169605f350f8d30e40e39430
Charles Quiros <charles.quiro@gmail.com>
2021-12-03 12:30:41 -0800

    created the store obj and have done a basic wire up of the functions. Need to implement the logic still

--- script.js
+++ script.js
@@ -2,39 +2,32 @@ const display = document.getElementById("display");
 const buttons = document.querySelectorAll("button");
 
 display.textContent = 0;
-
-let preStorageArray = [];
-let prevNumArray = [];
-let currentNumArray = [];
+let store = {
+  temp: "",
+  storeNum: false,
+  prevNum: "",
+  currentNum: "",
+  operator: null,
+};
 
 buttons.forEach((button) => {
   button.addEventListener("click", (e) => operate(e));
 });
 
 const storeNums = (e) => {
-  if (e.target.className === "numbers") {
-    let userNum = e.target.textContent;
+  const numClicked = e.target.textContent;
 
-    preStorageArray.push(userNum);
-    const joinedNums = preStorageArray.join("");
-    display.textContent = joinedNums;
-
-    if (localStorage.operator) {
-      prevNumArray.push(joinedNums);
-      localStorage.prevNumArray = JSON.stringify(prevNumArray);
-      localStorage.removeItem("operator");
-    }
+  if (store.storeNum) {
+    console.log("storeNum");
   }
 };
 
+//Only when an operator is clicked, the number entered by the user is finally stored in the local storage
 const storeOperators = (e) => {
-  if (e.target.className === "operators") {
-    const userOperator = e.target.textContent;
-    localStorage.setItem("operator", userOperator);
-    //TODO remove this
-    display.textContent = e.target.textContent;
-  }
+  store.storeNum = true;
 };
+
+const storeExtra = (e) => {};
 
 const calculate = (num1, num2, storedOperator) => {
   switch (storedOperator) {
@@ -55,27 +48,16 @@ const calculate = (num1, num2, storedOperator) => {
   }
 };
 
-const storeExtra = (e) => {};
-
 const operate = (e) => {
-  storeNums(e);
-
-  storeOperators(e);
-
-  storeExtra(e);
-
-  if (e.target.id === "equals") {
-    const storedValues = JSON.parse(localStorage.userValues);
-    const storedOperator = localStorage.getItem("operator");
-
-    const arrayLength = storedValues.length;
-    const num1 = parseInt(storedValues[arrayLength - 2], 10);
-    const num2 = parseInt(storedValues[arrayLength - 1], 10);
-
-    const result = calculate(num1, num2, storedOperator);
-
-    userValues.push(String(result));
-    localStorage.userValues = JSON.stringify(userValues);
-    display.textContent = result;
+  if (e.target.className === "numbers") {
+    storeNums(e);
+  } else if (e.target.className === "operators") {
+    storeOperators(e);
+  } else if (e.target.className === "extra") {
+    storeExtra(e);
+  } else if (e.target.id === "period") {
+    console.log("period");
+  } else if (e.target.id === "equals") {
+    console.log("equals");
   }
 };
